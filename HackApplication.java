ffac21bb5c477060147b5d86404ce90086563335
Teledar <lmartin1920@outlook.com>
2024-12-10 20:36:46 -0500

    Add files via upload

--- HackApplication.java
+++ HackApplication.java
@@ -1,15 +1,33 @@
-public class HackApplication {
+public final class HackApplication {
 
     //Hack .vm files will be converted to a HackApplication.class file when compiled. Execution will begin in the run() function.
+    //This is a sample file to show the underlying structure of the generated .class file
 
-    //Pointer to the beginning of the screen memory map
-    final static int SCREEN = 16384 - 2048;
+    //a reference to the ram
+    private static short ram[];
 
-    //Pointer to the keyboard memory map
-    final static int KBD = 24576 - 2048;
+    //temp 0 through temp 7
+    private static short temps[];
 
-    public static void run(Short ram[]) {
-        ram[SCREEN + 32] = -32768;
-        ram[SCREEN + 33] = -32768;
+    //static 0 through static 239
+    private static short statics[];
+
+    //the original name of the Hack program
+    public static String getName() {
+        //Name is auto-generated from the directory that the VM files were in
+        return "Pong";
     }
+
+    public static void run(short ram_array[]) {
+        ram = ram_array;
+        temps = new short[8];
+        statics = new short[240];
+        Sys_init();
+    }
+
+    //Generated from Hack VM code
+    private static short Sys_init() {
+        //Application logic starts here
+    }
+
 }
