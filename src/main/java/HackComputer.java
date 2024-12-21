413336442b3241759e250d4203718ca493b27dc5
Teledar <lmartin1920@outlook.com>
2024-12-21 11:49:59 -0500

    add HEAP_START constant

--- src/main/java/HackComputer.java
+++ src/main/java/HackComputer.java
@@ -1,13 +1,20 @@
 
 public class HackComputer {
 
-    //Pointer to the beginning of the screen memory map
+    // Pointer to the beginning of the screen memory map
     public static final short SCREEN = 16384;
 
-    //Pointer to the keyboard memory map
+    // Pointer to the keyboard memory map
     public static final short KBD = 24576;
 
+    // This can be increased up to 32767 if you need more RAM
     public static final short RAM_SIZE = KBD + 1; 
+
+    // The Nand2Tetris VM emulator does not permit direct access to addresses below 2048,
+    // since these are used for the stack and static variables. With JHack, the JVM handles
+    // the stack and static variables, so the first 2048 addresses are unused. If you want
+    // to make them available for your system to use as heap, change this value to 0.
+    public static final short HEAP_START = 2048;
 
     public static final short TEMPS_SIZE = 8;
 
@@ -32,14 +39,14 @@ public class HackComputer {
     }
 
     public static short peek(short address) {
-        if (address < 0 || address >= RAM_SIZE) {
+        if (address < HEAP_START || address >= RAM_SIZE) {
             //throw exception
         }
         return ram[address];
     }
 
     public static void poke(short value, short address) {
-        if (address < 0 || address >= RAM_SIZE) {
+        if (address < HEAP_START || address >= RAM_SIZE) {
             //throw exception
         }
         ram[address] = value;
